47112f01428e1c1412872ea3d9993ebf2562d91e
tecnarchico <37384647+tecnarchico@users.noreply.github.com>
2024-05-17 17:54:52 +0100

    Tests sin

--- tinygrad/function.py
+++ tinygrad/function.py
@@ -38,11 +38,20 @@ class Reciprocal(Function):
 
 class Sin(Function):
   def forward(self, x:LazyBuffer) -> LazyBuffer:
-    self.x = x
-    return x.e(UnaryOps.SIN)
+    # normalize x with analogue of math.fmod 2*pi, the double cast is to get the floor
+    self.x =  x.e(BinaryOps.SUB, x.e(BinaryOps.DIV, x.const(math.pi*2)).cast(dtypes.int).cast(x.dtype).e(BinaryOps.MUL, x.const(math.pi*2)))
+    ret = acc = self.x
+    for i in range(1, 13):
+      acc = acc.e(UnaryOps.NEG).e(BinaryOps.MUL, self.x).e(BinaryOps.MUL, self.x)
+      ret = ret.e(BinaryOps.ADD, acc.e(BinaryOps.DIV, self.x.const(math.factorial(2*i + 1))))
+    return ret
 
   def backward(self, grad_output:LazyBuffer) -> LazyBuffer:
-    return self.x.const(math.pi / 2).e(BinaryOps.SUB, self.x).e(UnaryOps.SIN).e(BinaryOps.MUL, grad_output)
+    ret = acc = self.x.const(1)
+    for i in range(1, 13):
+      acc = acc.e(UnaryOps.NEG).e(BinaryOps.MUL, self.x).e(BinaryOps.MUL, self.x)
+      ret = ret.e(BinaryOps.ADD, acc.e(BinaryOps.DIV, self.x.const(math.factorial(2*i))))
+    return grad_output.e(BinaryOps.MUL, ret)
 
 # NOTE: maximum(x, 0) behaves differently where x=0
 class Relu(Function):
